0b44629a6ec5a4d876a399697ad19dc23672409c
Nakuri <ccmuk58@naver.com>
2022-10-05 01:24:36 +0900

    Create BOJ_2738.cpp

---
+++ BOJ_2738.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int a[102][102], b[102][102];
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> b[i][j];
+            a[i][j] += b[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
