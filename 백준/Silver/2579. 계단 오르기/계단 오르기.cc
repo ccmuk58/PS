3a3a8c57af8ff0e5fc7798df0b5feae0d13f44ba
Nakuri <ccmuk58@gmail.com>
2023-02-01 05:25:52 +0900

    Update 계단 오르기.cc

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,32 +2,36 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
+constexpr int RANGE = 302;
+
+int GetMaxDP(int dp[3]) {
+	int max = (dp[1] > dp[2]) ? dp[1] : dp[2];
+	return max;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-	int stair[302] = { 0 };
-	int dp[302][3] = { 0 };
-	int n;
-	int max = 0;
+	// 최대 값을 기억할 변수
+	int dp[RANGE][3] = { 0 };
+	int stair[RANGE] = { 0 };
+	int n = 0;
 
 	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> stair[i];
+	for (int i = 1; i <= n; i++) 
+		cin >> stair[i];
 
+	// 초기값
 	dp[1][1] = stair[1];
 	for (int i = 2; i <= n; i++)
 	{
-		if (dp[i - 2][1] > dp[i - 2][2])
-			dp[i][1] = dp[i - 2][1] + stair[i];
-		else
-			dp[i][1] = dp[i - 2][2] + stair[i];
-
+		// 1회 연속 이동의 경우, -2계단의 이동 값 중 큰 값을 더함
+		dp[i][1] = GetMaxDP(dp[i - 2]) + stair[i];
+		// 2회 연속 이동의 경우, -1계단의 1회 연속 이동 값을 더함
 		dp[i][2] = dp[i - 1][1] + stair[i];
 	}
 
-	if (dp[n][1] > dp[n][2]) max = dp[n][1];
-	else max = dp[n][2];
-	cout << max;
+	cout << GetMaxDP(dp[n]);
 }
