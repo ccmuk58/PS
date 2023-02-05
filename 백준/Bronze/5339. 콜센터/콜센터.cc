8fe0939af1c0d1138b39b2a9a7e4e57ae1b9e3a9
Nakuri <ccmuk58@gmail.com>
2023-02-05 17:04:18 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |");
+}
