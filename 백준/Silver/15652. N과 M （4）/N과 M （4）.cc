c5f42d5d62d1011fe067a60c7fac58121139852f
Nakuri <ccmuk58@gmail.com>
2023-02-07 19:59:36 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+constexpr int LIMIT = 10;
+
+int N, M;
+int num[LIMIT];
+bool visited[LIMIT];
+
+void solve(int cnt) {
+    if (cnt > M) {
+        for (int i = 1; i <= M; i++) {
+            cout << num[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+		if(i < num[cnt-1]) continue;
+		visited[i] = true;
+		num[cnt] = i;
+		solve(cnt + 1);
+		visited[i] = false;
+	}
+}
+
+int main() {
+    cin >> N >> M;
+    solve(1);
+}
