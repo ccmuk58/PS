6523c687a9e948fba8ef6665c1d2095701f9e9f6
Nakuri <ccmuk58@gmail.com>
2023-02-10 00:14:33 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 2648 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+constexpr int LIMIT = 100000;
+
+struct Info
+{
+	int x;
+	int cnt;
+};
+int N, K;
+bool visited[LIMIT + 2];
+int ans;
+
+void BFS()
+{
+	queue<Info> q;
+	q.push({ N, 0 });
+
+	while (!q.empty())
+	{
+		auto [x, cnt] = q.front();
+		q.pop();
+
+		if (x == K)
+		{
+			ans = cnt;
+			return;
+		}
+
+		if (x < 0 || x > LIMIT) continue;
+		if (visited[x]) continue;
+		visited[x] = true;
+		q.push({ x * 2 , cnt });
+		q.push({ x - 1, cnt + 1 });
+		q.push({ x + 1, cnt + 1 });
+	}
+}
+
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+
+	cin >> N >> K;
+
+	BFS();
+
+	cout << ans;
+}
