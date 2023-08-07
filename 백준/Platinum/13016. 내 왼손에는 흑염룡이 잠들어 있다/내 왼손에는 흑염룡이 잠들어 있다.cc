50f8ad2d327ec2f9cea0ccca7820c79ee3f979f0
Janghyun Choi <ccmuk58@gmail.com>
2023-08-07 21:32:47 +0900

    [Platinum V] Title: 내 왼손에는 흑염룡이 잠들어 있다, Time: 32 ms, Memory: 5536 KB … …-BaekjoonHub

--- 백준/Platinum/13016. 내 왼손에는 흑염룡이 잠들어 있다/내 왼손에는 흑염룡이 잠들어 있다.cc
+++ 백준/Platinum/13016. 내 왼손에는 흑염룡이 잠들어 있다/내 왼손에는 흑염룡이 잠들어 있다.cc
@@ -7,46 +7,71 @@ struct Node {
     int dist;
 };
 int N;
-vector<Node> graph[LIMIT];
+vector<Node> tree[LIMIT];
 bool visited[LIMIT];
 int result[LIMIT];
 int startNode;
-int maxSum;
+int maxDistSum;
 
-void dfs(int idx, int distSum=0)
+void Print()
 {
-    visited[idx] = true;
-    if (distSum > maxSum)
+    for (int i = 1; i <= N; i++)
+        cout << result[i] << "\n";
+}
+
+void CheckLeaf(int idx, int distSum)
+{
+    if (distSum > maxDistSum)
     {
-        maxSum = distSum;
+        maxDistSum = distSum;
         startNode = idx;
     }
+}
+void dfs(int idx, int distSum=0)
+{
+    CheckLeaf(idx, distSum);
     result[idx] = max(result[idx], distSum);
-    for (int i = 0; i < graph[idx].size(); i++)
+
+    for (int i = 0; i < tree[idx].size(); i++)
     {
-        auto& [nIdx, dist] = graph[idx][i];
+        auto& [nIdx, dist] = tree[idx][i];
         if (visited[nIdx]) continue;
         visited[nIdx] = true;
         dfs(nIdx, distSum+dist);
     }
 }
 
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-
-    cin >> N;
-    for (int i = 1; i <= N-1; i++) {
-        int from, to, dist;
-        cin >> from >> to >> dist;
-        graph[from].push_back({ to, dist });
-        graph[to].push_back({ from, dist });
-    }
-    dfs(1);
+void SearchDistance()
+{
     for (int i = 0; i < 2; i++)
     {
         memset(visited, 0, sizeof(visited));
         dfs(startNode);
     }
-    for (int i = 1; i <= N; i++)
-        cout << result[i] << "\n";
+}
+
+void SearchFirstLeaf()
+{
+    visited[1] = true;
+    dfs(1);
+}
+
+void MakeTree()
+{
+    cin >> N;
+    for (int i = 1; i <= N-1; i++) {
+        int from, to, dist;
+        cin >> from >> to >> dist;
+        tree[from].push_back({to, dist });
+        tree[to].push_back({from, dist });
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    MakeTree();
+    SearchFirstLeaf();
+    SearchDistance();
+    Print();
 }
