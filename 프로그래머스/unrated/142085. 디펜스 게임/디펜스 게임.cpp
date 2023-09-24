2cf80c3e08384b92fe8ce9d8dbd9a095d1592b03
Janghyun Choi <ccmuk58@gmail.com>
2023-09-24 21:36:48 +0900

    [unrated] Title: 디펜스 게임, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
@@ -5,27 +5,33 @@ using namespace std;
 
 int solution(int n, int k, vector<int> enemy) {
     int answer = 0;
-    	
+    // 막은 적을 내림차순으로 저장
 	priority_queue<int> pq;
     
 	while(answer < enemy.size())
 	{
 		pq.push(enemy[answer]);
 		n -= enemy[answer];
-		if(n<0)
-		{
-			if(k>0)
-			{
-				--k;
-				n += pq.top();
-				pq.pop();
-			}
-			else
-			{
-				return answer;
-			}
-		}
-		++answer;
+        
+        // 막은 후 체력 0이상이면 다음 라운드로
+		if(n>=0)
+        {
+		    ++answer;
+            continue;            
+        }
+        // 0보다 작은데 무적권 없으면 gameover
+        else if(k<=0) 
+        {
+            return answer;
+        }
+        // 무적권으로 지난 라운드 중 가장 큰 라운드 무적 처리
+        else
+        {
+            --k;
+            n += pq.top();
+            pq.pop();
+            ++answer;   
+        }
 	}
     return answer;
 }
