c3db836e1e14553f0748d89545a0252564d69cc9
Janghyun Choi <ccmuk58@gmail.com>
2023-11-20 19:43:42 +0900

    [Platinum IV] Title: 달리기, Time: 500 ms, Memory: 25524 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.cc
+++ 백준/Platinum/2517. 달리기/달리기.cc
@@ -4,16 +4,19 @@
 using namespace __gnu_pbds;
 using namespace std;
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+ordered_set oSet;
+
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
-	ordered_set OS;
+
 	int n, t;
 	cin >> n;
 	for(int i=1; i<=n; i++)
 	{
 		cin >> t;
-		OS.insert(t);
-		cout << (i - OS.order_of_key(t)) << " ";
+		oSet.insert(t);
+		cout << (i - oSet.order_of_key(t)) << " ";
 	}
 }
