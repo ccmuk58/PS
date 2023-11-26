8eb2c88b9c5893a40e74e2826c434b02915a7c5c
Janghyun Choi <ccmuk58@gmail.com>
2023-11-26 22:06:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 228 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	int dp[n+1];	
+	for (int i = 0; i < n; i++) cin >> v[i];
+	dp[0] = v[0];
+	int ans = 1;
+	for(int i=1; i<n; i++)
+	{
+		if(v[i] > dp[ans-1])
+		{
+			dp[ans++]  = v[i];
+		}
+		else{
+			*lower_bound(dp, dp+ans, v[i]) = v[i];
+		}
+	}
+	cout << ans;
+}
