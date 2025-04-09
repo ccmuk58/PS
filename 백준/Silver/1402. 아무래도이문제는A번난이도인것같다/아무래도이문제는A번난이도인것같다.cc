ef885e0385bbaf69c5bda7272ed77e4dd1e63969
Janghyun Choi <ccmuk58@gmail.com>
2025-04-09 16:49:36 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1};
+constexpr int dx[] = {-1, +1, 0, 0};
+
+constexpr int LMT = 1e6 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+	cin >> T;
+	while(T--)
+	{
+		cin >> A >> B;
+		cout << "yes\n";
+	}
+}
