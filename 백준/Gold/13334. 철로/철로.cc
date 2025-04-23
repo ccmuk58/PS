f4bd91c216dce6cb6545d628b056d4766bff4256
Janghyun Choi <ccmuk58@gmail.com>
2025-04-23 20:19:44 +0900

    [Gold II] Title: 철로, Time: 40 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/13334. 철로/철로.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1};
+constexpr int dx[] = {-1, +1, 0, 0};
+constexpr int LMT = 1e6 + 2;
+constexpr ll MOD = 1'000'000'007;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int ans = 0;
+vector<pl> v;
+bool cmp(pl a, pl b){
+    if(a.first == b.first) 
+        return a.second < b.second;
+    else 
+        return a.first < b.first;
+}
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> N;
+
+	for(int i=0; i<N; i++)
+	{
+		cin >> H >> O;
+		if(H>O) swap(H, O);
+		v.push_back({O, H});
+	}
+	cin >> D;
+
+	sort(v.begin(), v.end(), cmp);
+
+	priority_queue<int> pq;
+	int i=0;
+	while(i<N)
+	{
+		auto st = v[i].second;
+		auto end = v[i].first;
+		if(st >= end-D)
+			pq.push(-st);
+
+		while(!pq.empty())
+		{
+			if(-pq.top() >= end - D)
+				break;
+			pq.pop();
+			
+		}
+		ans = max(ans, (int)pq.size());
+		i++;
+	}
+	cout << ans;
+}
