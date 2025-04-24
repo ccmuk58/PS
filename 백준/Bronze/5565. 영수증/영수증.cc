f0ee4f62f3d044335aa8457f6f05d2a96d05c57b
Janghyun Choi <ccmuk58@gmail.com>
2025-04-24 23:25:12 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1};
+constexpr int dx[] = {-1, +1, 0, 0};
+constexpr int LMT = 1e6 + 2;
+constexpr ll MOD = 1'000'000'007;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int ans = 0;
+vector<int> v;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+	cin >> T;
+
+	for(int i=0; i<9; i++)
+	{
+		cin >> A;
+		T-=A;
+	}
+	cout << T;
+}
