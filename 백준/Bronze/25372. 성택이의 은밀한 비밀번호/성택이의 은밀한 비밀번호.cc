032d3040aaad98501b069d88fde34c7e0d3500a9
Janghyun Choi <ccmuk58@gmail.com>
2025-06-08 00:18:35 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+constexpr int dy[] = {0, 0, -1, +1};
+constexpr int dx[] = {-1, +1, 0, 0};
+int A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> T;
+
+	while(T--)
+	{
+		string s;
+		cin >> s;
+
+		if(s.size()>=6 && s.size()<=9)
+			cout << "yes\n";
+		else 
+			cout << "no\n";
+	}
+}
