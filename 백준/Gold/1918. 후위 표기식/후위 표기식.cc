1b98f559fb10b6fc646b28d818e71b35517f5ca4
Janghyun Choi <ccmuk58@gmail.com>
2025-09-15 01:57:08 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+ll ans = 0;
+vector<ll> v;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	string s;
+
+	cin >> s;
+
+	string as;
+
+	stack<char> st;
+	for(int i=0; i<s.length(); i++)
+	{
+
+		if(s[i]>='A' && s[i]<='Z')
+		{
+			cout << s[i];
+		}
+		else if(s[i]=='+' || s[i]=='-')
+		{
+			while(!st.empty() && (st.top() == '*' || st.top() == '/'|| st.top() == '+'|| st.top() == '-'))
+			{
+				cout << st.top();
+				st.pop();
+			}
+			st.push(s[i]);
+		}
+		else if(s[i]=='*' || s[i]=='/')
+		{
+			while(!st.empty() && (st.top() == '*' || st.top() == '/'))
+			{
+				cout << st.top();
+				st.pop();
+			}
+			st.push(s[i]);
+		}
+		else if(s[i]=='(')
+		{
+			st.push(s[i]);
+		}
+		else if(s[i]==')')
+		{
+			while(!st.empty() && st.top()!='(')
+			{
+				cout << st.top();
+				st.pop();
+			}
+			st.pop();
+		}
+	}
+
+	while(!st.empty())
+	{
+		cout << st.top();
+		st.pop();
+	}
+
+}
