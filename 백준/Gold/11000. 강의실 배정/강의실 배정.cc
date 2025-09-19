a13a29980aa43044e0005ac2ebe6c8f458a90dd7
Janghyun Choi <ccmuk58@gmail.com>
2025-09-20 02:28:22 +0900

    [Gold IV] Title: 강의실 배정, Time: 56 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+ll ans = 0;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> N;
+	vector<pi> v;
+	for(int i=0; i<N; i++)
+	{
+		cin >> S >> T;
+		v.push_back({S,T});
+	}
+
+	sort(v.begin(), v.end());
+
+	priority_queue<int> pq;
+
+	for(int i=0; i<N; i++)
+	{
+		while(!pq.empty() && -pq.top()<=v[i].first)
+		{
+			pq.pop();
+		}
+		pq.push(-v[i].second);
+		ans = max(ans, (ll)pq.size());
+	}
+
+	cout << ans;
+
+}
