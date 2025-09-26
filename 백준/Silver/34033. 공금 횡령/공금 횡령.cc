b18f4d7c0f6fc5a17ada93fdff257eb35516e0e5
Janghyun Choi <ccmuk58@gmail.com>
2025-09-26 17:14:46 +0900

    [Silver IV] Title: 공금 횡령, Time: 152 ms, Memory: 9952 KB -BaekjoonHub

---
+++ 백준/Silver/34033. 공금 횡령/공금 횡령.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll,ll>;
+using pll = pair<ll, pair<ll,ll>>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> N >> M;
+
+	map<string, int> m;
+	ll ans=0;
+	for(int i=0; i<N; i++)
+	{
+		string s;
+		cin >> s >> A;
+		m[s]=A;
+	}
+
+	for(int i=0; i<M; i++)
+	{
+		string s;
+		cin >> s >> A;
+
+		if((double)m[s]*1.05 < A)
+		{
+			ans++;
+		}
+
+	}
+	cout << ans;
+
+}
