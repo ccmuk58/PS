20b91c2f7c43b6b0c0627d00548105932d874065
Janghyun Choi <ccmuk58@gmail.com>
2025-10-02 18:58:22 +0900

    [Gold V] Title: 의좋은 형제, Time: 32 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/32406. 의좋은 형제/의좋은 형제.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+	cin >> N;
+	vector<ll> a, b;
+
+	for(int i=0; i<N; i++)
+	{
+		cin >> A;
+		a.push_back(A);
+	}
+
+	
+	for(int i=0; i<N; i++)
+	{
+		cin >> A;
+		b.push_back(A);
+	}
+
+	if(a[N-1]+b[N-2] < b[N-1] + a[N-2])
+	{
+		swap(a, b);
+	}
+	for(int i=0; i<N-2; i++)
+	{
+		if(a[i] >= b[i])
+		{
+			a[N-2] += b[i];
+			b[N-2] += a[i];
+		}
+		else
+		{
+			a[N-1] += b[i];
+			b[N-1] += a[i];
+		}
+	}
+
+	a[N-1]+=b[N-2];
+	b[N-1]+=a[N-2];
+	cout << a[N-1] - b[N-1];
+}
