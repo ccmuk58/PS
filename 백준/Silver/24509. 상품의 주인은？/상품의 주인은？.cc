4b236bd4402e7ea12947fdadf64f767f94b9a87f
Janghyun Choi <ccmuk58@gmail.com>
2025-11-11 01:52:12 +0900

    [Silver V] Title: 상품의 주인은?, Time: 144 ms, Memory: 11380 KB -BaekjoonHub

---
+++ 백준/Silver/24509. 상품의 주인은？/상품의 주인은？.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll,ll>;
+using pll = pair<ll, pair<ll,ll>>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+ll ans = 0;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> N;
+	vector<pi> v[4];
+	for(int i=0; i<N; i++)
+	{
+		cin >> A;
+		cin >> Q>>W>>E>>R;
+		v[0].push_back({Q,-A});
+		v[1].push_back({W,-A});
+		v[2].push_back({E,-A});
+		v[3].push_back({R,-A});
+	}
+
+	for(int i=0; i<4; i++)
+		sort(v[i].begin(), v[i].end(), greater<>());
+
+	bool received[N+2] = {};
+	for(int i=0; i<4; i++)
+	{
+		int j=0;
+		while(received[-v[i][j].second])
+		{
+			j++;
+		}
+		received[-v[i][j].second]=true;
+		cout << -v[i][j].second << " ";
+	}
+
+
+
+}
