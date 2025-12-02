99d2c4323bc245ca602e1aad641b0af8b8b3a103
Janghyun Choi <ccmuk58@gmail.com>
2025-12-02 20:11:38 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pll = pair<ll, pair<ll, ll>>;
+using plp = pair<ll, pl>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+ll ans = INT64_MAX;
+
+int dp[100002];
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+	cin >> N;
+
+	iota(dp+1, dp+N+1, 1);
+
+	for(int i=1; i<=N; i++)
+	{
+		for(int j=1; j*j<=i; j++)
+		{
+			dp[i] = min(dp[i-j*j]+1, dp[i]);
+		}
+	}
+    cout << dp[N];
+}
+/*
+18 
+9 9
+16 1 1
+*/
