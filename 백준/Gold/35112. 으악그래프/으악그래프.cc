6dd5f3958d3b8702f01fd152df38c3dbb75ba337
Janghyun Choi <ccmuk58@gmail.com>
2026-01-15 17:02:08 +0900

    [Gold V] Title: 으악그래프, Time: 680 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/35112. 으악그래프/으악그래프.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    
+    for(int i=0; i<M; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+    }
+    
+    if(M<=N)cout << "Yes";
+    else cout << "No";
+}
