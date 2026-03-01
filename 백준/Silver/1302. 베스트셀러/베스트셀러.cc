79d8c4b34cf3a4d8eefa2fd62a04e5cc37a1d3d5
Janghyun Choi <ccmuk58@gmail.com>
2026-03-02 01:03:16 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll,ll>;
+using pll = pair<ll, pair<ll,ll>>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, +1, +1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LMT = 1e5 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+map<string, int> m;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> T;
+	string anss = "";
+	while(T--)
+	{
+		string s;
+		cin >> s;
+		m[s]++;
+		if(m[s]>m[anss])
+		{
+			anss = s;
+		}
+		else if(m[s]==m[anss])
+		{
+			if(s<anss)
+			{
+				anss=s;
+			}
+		}
+	}
+	cout << anss;
+
+}
