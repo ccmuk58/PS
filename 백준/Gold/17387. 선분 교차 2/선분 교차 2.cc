97626b1c32717d3ff8ecf22e50cee75672fd99ae
Janghyun Choi <ccmuk58@gmail.com>
2026-03-21 14:27:15 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,41 +1,41 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
-using namespace __gnu_pbds;
 using pi = pair<int, int>;
 using ll = long long;
 using pl = pair<ll, ll>;
 using pip = pair<int, pi>;
 using pdp = pair<double, pi>;
-constexpr int dy[] = {0, 0, -1, +1};
-constexpr int dx[] = {-1, +1, 0, 0};
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, 1, 1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, 1, -1, 1};
 constexpr int LMT = 1e6 + 2;
-constexpr ll MOD = 1'000'000'007;
 ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
 
-pl p1, p2;
-pl p3, p4;
-
-ll ccw(pl a, pl b, pl c)
+struct Point
 {
-	pl v1 = {b.first-a.first, b.second-a.second};
-	pl v2 = {c.first-b.first, c.second-b.second};
-	ll w = v1.first*v2.second - v1.second*v2.first;
-	if(w < 0) return -1;
-	else if(w > 0) return 1;
-	else return 0;
+	ll x, y;
+	auto operator<=>(const Point&) const = default;
+};
+
+Point p1,p2,p3,p4;
+
+ll ccw(Point a, Point b, Point c)
+{
+	Point v1 = {b.x-a.x, b.y-a.y};
+	Point v2 = {c.x-b.x, c.y-b.y};
+	ll w = v1.x*v2.y - v1.y*v2.x;
+	if(w < 0) return -1;	// cw
+	else if(w > 0) return 1; // ccw
+	else return 0;	// 일직선
 }
 ll ans=0;
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
 	
-	cin >> p1.first >> p1.second;
-	cin >> p2.first >> p2.second;
-	cin >> p3.first >> p3.second;
-	cin >> p4.first >> p4.second;
+	cin >> p1.x >> p1.y;
+	cin >> p2.x >> p2.y;
+	cin >> p3.x >> p3.y;
+	cin >> p4.x >> p4.y;
 
 	ll a = ccw(p1, p2, p3) * ccw(p1, p2, p4);
 	ll b = ccw(p3, p4, p1) * ccw(p3, p4, p2);
