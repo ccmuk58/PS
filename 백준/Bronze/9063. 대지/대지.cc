b25b5e3829df70775f7c4aacbdc4551793bc0649
Janghyun Choi <ccmuk58@gmail.com>
2026-03-21 13:44:16 +0900

    [Bronze III] Title: 대지, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
+using pl = pair<ll, ll>;
+using pip = pair<int, pi>;
+using pdp = pair<double, pi>;
+constexpr int dy[] = {0, 0, -1, +1, -1, -1, 1, 1};
+constexpr int dx[] = {-1, +1, 0, 0, -1, 1, -1, 1};
+constexpr int LMT = 1e6 + 2;
+ll A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	
+	cin >> N;
+	ll ans =0;
+	vector<ll> v;
+	ll arr[N+2]={};
+	ll minX = 1000000;
+	ll minY = 1000000;
+	ll maxX = -1000000;
+	ll maxY = -1000000;
+	for(int i=0; i<N; i++)
+	{
+		cin >> X >> Y;
+		maxX = max(X, maxX);
+		maxY = max(Y, maxY);
+		minX = min(X, minX);
+		minY = min(Y, minY);
+	}
+
+	cout << abs(maxX-minX) * abs(maxY-minY);
+}
